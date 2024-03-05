2963fc207c9f237b726509df07a391919e5c17a3
naxsyambis <naxsyambis@gmail.com>
2024-03-05 14:13:40 +0000

    Add project files.

---
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,29 @@
+// ConsoleApplication3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int bilangan;
+    srand(time(0));
+
+    bilangan = rand() % 10;
+
+    cout << "bilangan awal = " << bilangan << endl;
+
+    if (bilangan >= 5) {
+   // yang didalam kurung namanya operator
+        bilangan = 2 * bilangan;
+    }
+
+    cout << "bilangan akhir = " << bilangan << endl;
+    return 0;
+
+    std::cout << "Hello World!\n";
+}
+
+
+
+
